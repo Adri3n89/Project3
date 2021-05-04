0ba3e8ae891682452fe3004a2373cb79c7e80982
Adri3n89 <adrien@perea.be>
2021-05-04 10:58:45 +0200

    Update FightManager.swift

--- Project3/Model/FightManager.swift
+++ Project3/Model/FightManager.swift
@@ -22,43 +22,6 @@ var player1 = Player()
 var player2 = Player()
 
 // MARK: - FUNCTIONS
-func isGameOver() {
-    if checkHealth(player1, 0) == 0 && checkHealth(player1, 1) == 0 && checkHealth(player1, 2) == 0 {
-        game.winner = player2
-        game.state = .isOver
-    }
-    if checkHealth(player2, 0) == 0 && checkHealth(player2, 1) == 0 && checkHealth(player2, 2) == 0 {
-        game.state = .isOver
-        game.winner = player1
-    }
-}
-func attack() {
-    currentC!.attack(ennemy: currentTarget!)
-    // set life on 0 ( cannot be negative )
-    if (currentTarget?.race.health)! < 0 {
-        currentTarget?.race.health = 0
-    }
-}
-func heal() {
-    currentC!.heal(ally: currentTarget!)
-    // set life on max even if it heal more
-    if (currentTarget?.race.health)! > (currentTarget?.race.healthMax)! {
-        currentTarget?.race.health = (currentTarget?.race.healthMax)!
-    }
-}
-func checkTurn() {
-    var characterPlayed = 0
-    for character in characterArray where character.canPlay == false {
-        characterPlayed += 1
-    }
-    if characterPlayed == 6 {
-        game.totalTurn += 1
-        currentPIndex = 0
-        for character in characterArray where character.race.health > 0 {
-            character.canPlay = true
-        }
-    }
-}
 func letsBounce(button: UIButton) {
     button.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
     UIView.animate(withDuration: 0.5) {button.transform = CGAffineTransform.identity}
@@ -68,27 +31,6 @@ func letsBounce(button: UIButton) {
 func activeButton(button: UIButton, active: Bool, alpha: Double) {
     button.isEnabled = active
     button.alpha = CGFloat(alpha)
-}
-// convert the type race of characters to an emoji for the label button
-func convertRace(charac: Character) -> String {
-        var race = ""
-    if charac.race.type.rawValue == "elf" {
-            race = "🧝"
-        }
-    if charac.race.type.rawValue == "human" {
-            race = "👨"
-        }
-    if charac.race.type.rawValue == "wizzard" {
-            race = "🧙‍♂️"
-        }
-    if charac.race.type.rawValue == "dwarf" {
-            race = "🎅"
-        }
-        return race
-    }
-
-func checkHealth(_ player: Player, _ index: Int) -> Int {
-    return player.characters[index].race.health
 }
 
 func checkHealCharacter(player: Player, indexCurrentCharac: Int, coop1: Int, coop2: Int, currentCharacButton: UIButton, coop1Button: UIButton, coop2Button: UIButton) {
@@ -102,26 +44,6 @@ func checkHealCharacter(player: Player, indexCurrentCharac: Int, coop1: Int, coo
         if checkHealth(player, coop2) > 0 && checkHealth(player, coop2) < player.characters[coop2].race.healthMax {
             activeButton(button: coop2Button, active: true, alpha: 1)
         }
-    }
-}
-
-func randomChest(view: UIViewController) {
-    let randomNumber2: Int = .random(in: 0...4)
-    let randomNumber: Int = .random(in: 0...2)
-    if randomNumber2 == 2 {
-    switch currentC!.race.type {
-    case .elf : randomWeapon = arrayBow[randomNumber]
-    case .dwarf : randomWeapon = arrayAxe[randomNumber]
-    case .human : randomWeapon = arraySword[randomNumber]
-    case .wizzard : randomWeapon = arrayStick[randomNumber]
-    }
-    currentC!.race.weapon = randomWeapon!
-    // probleme affiche les bons dégats dans la console mais pas dans l'alerte
-    print("\(randomWeapon!) + \(randomWeapon!.damage) + \(randomWeapon!.heal)")
-    let alertController = UIAlertController(title: "🎁", message: "A treasure chest appears with a \(randomWeapon!.name) inside\ndamage : \(randomWeapon!.damage) heal : \(randomWeapon!.heal)", preferredStyle: .alert)
-        let presentChest = UIAlertAction(title: okString, style: .default, handler: {_ in })
-    alertController.addAction(presentChest)
-    view.present(alertController, animated: true)
     }
 }
 
@@ -140,72 +62,11 @@ func setupFightView(characterArray: [Character], nameLabelArray: [UILabel], hpLa
     }
 }
 
-private func characterIsDead(character: Character, buttonCharacterArray: [UIButton]) {
-    if character.race.health == 0 {
-        character.canPlay = false
-        if character == player1.characters[0] {
-            buttonCharacterArray[0].setTitle("💀", for: .normal)
-        } else if character == player1.characters[1] {
-            buttonCharacterArray[1].setTitle("💀", for: .normal)
-        } else if character == player1.characters[2] {
-            buttonCharacterArray[2].setTitle("💀", for: .normal)
-        } else if character == player2.characters[0] {
-            buttonCharacterArray[3].setTitle("💀", for: .normal)
-        } else if character == player2.characters[1] {
-            buttonCharacterArray[4].setTitle("💀", for: .normal)
-        } else if character == player2.characters[2] {
-            buttonCharacterArray[5].setTitle("💀", for: .normal)
-        }
-    }
-}
-
-// function for refresh the health of all characters
-func refresh(characterArray: [Character], hpLabelArray: [UILabel], buttonCharacterArray: [UIButton]) {
-    for index in 0...5 {
-        hpLabelArray[index].text = String(characterArray[index].race.health)
-    }
-    currentAction = ""
-    currentTarget = nil
-    currentC = nil
-    for index in 0...2 {
-        characterIsDead(character: player1.characters[index], buttonCharacterArray: buttonCharacterArray)
-        characterIsDead(character: player2.characters[index], buttonCharacterArray: buttonCharacterArray)
-    }
-}
-
 func chooseCharacterOrTarget(choice: Character) {
     if currentAction == "" {
         currentC = choice
     } else {
         currentTarget = choice
-    }
-}
-
-func doAction(view: UIViewController, buttonArray: [UIButton], hpLabelArray: [UILabel], currentCLabel: UILabel, currentCHeal: UILabel, currentCAttack: UILabel) {
-    randomChest(view: view)
-    if currentAction == "attack" {
-        attack()
-    }
-    if currentAction == "heal" {
-        heal()
-    }
-    isGameOver()
-    print(game.state)
-    if game.state == .isOver {
-        view.performSegue(withIdentifier: "winView", sender: Any?.self)
-    } else {
-        currentC!.canPlay = false
-        currentPIndex += 1
-        checkTurn()
-        disableAllButton(buttonArray: buttonArray, index1: 0, index2: 5)
-        // change the index to go to the next character of the array
-        // if the last character play, go back to the first and increase a turn.
-    if currentPArray.count == currentPIndex {
-        currentPIndex = 0
-    }
-    currentP = currentPArray[currentPIndex]
-    refresh(characterArray: characterArray, hpLabelArray: hpLabelArray, buttonCharacterArray: buttonArray)
-    turn(allButtonArray: buttonArray, currentCLabel: currentCLabel, currentCHeal: currentCHeal, currentCAttack: currentCAttack, view: view, hpLabelArray: hpLabelArray)
     }
 }
 
@@ -258,5 +119,152 @@ func turn(allButtonArray: [UIButton], currentCLabel: UILabel, currentCHeal: UILa
     }
     if currentAction != "" && currentTarget != nil {
         doAction(view: view, buttonArray: allButtonArray, hpLabelArray: hpLabelArray, currentCLabel: currentCLabel, currentCHeal: currentCHeal, currentCAttack: currentCAttack)
+    }
+}
+
+// MARK: - PRIVATES FUNCTIONS
+private func characterIsDead(character: Character, buttonCharacterArray: [UIButton]) {
+    if character.race.health == 0 {
+        character.canPlay = false
+        if character == player1.characters[0] {
+            buttonCharacterArray[0].setTitle("💀", for: .normal)
+        } else if character == player1.characters[1] {
+            buttonCharacterArray[1].setTitle("💀", for: .normal)
+        } else if character == player1.characters[2] {
+            buttonCharacterArray[2].setTitle("💀", for: .normal)
+        } else if character == player2.characters[0] {
+            buttonCharacterArray[3].setTitle("💀", for: .normal)
+        } else if character == player2.characters[1] {
+            buttonCharacterArray[4].setTitle("💀", for: .normal)
+        } else if character == player2.characters[2] {
+            buttonCharacterArray[5].setTitle("💀", for: .normal)
+        }
+    }
+}
+
+// function for refresh the health of all characters
+private func refresh(characterArray: [Character], hpLabelArray: [UILabel], buttonCharacterArray: [UIButton]) {
+    for index in 0...5 {
+        hpLabelArray[index].text = String(characterArray[index].race.health)
+    }
+    currentAction = ""
+    currentTarget = nil
+    currentC = nil
+    for index in 0...2 {
+        characterIsDead(character: player1.characters[index], buttonCharacterArray: buttonCharacterArray)
+        characterIsDead(character: player2.characters[index], buttonCharacterArray: buttonCharacterArray)
+    }
+}
+
+private func doAction(view: UIViewController, buttonArray: [UIButton], hpLabelArray: [UILabel], currentCLabel: UILabel, currentCHeal: UILabel, currentCAttack: UILabel) {
+    randomChest(view: view)
+    if currentAction == "attack" {
+        attack()
+    }
+    if currentAction == "heal" {
+        heal()
+    }
+    characterIsDead(character: currentTarget!, buttonCharacterArray: buttonArray)
+    isGameOver()
+    if game.state == .isOver {
+        view.performSegue(withIdentifier: "winView", sender: Any?.self)
+    } else {
+        currentC!.canPlay = false
+        currentPIndex += 1
+        checkTurn()
+        disableAllButton(buttonArray: buttonArray, index1: 0, index2: 5)
+        // change the index to go to the next character of the array
+        // if the last character played, go back to the first
+    if currentPArray.count == currentPIndex {
+        currentPIndex = 0
+    }
+    currentP = currentPArray[currentPIndex]
+    refresh(characterArray: characterArray, hpLabelArray: hpLabelArray, buttonCharacterArray: buttonArray)
+    turn(allButtonArray: buttonArray, currentCLabel: currentCLabel, currentCHeal: currentCHeal, currentCAttack: currentCAttack, view: view, hpLabelArray: hpLabelArray)
+    }
+}
+
+private func isGameOver() {
+    if checkHealth(player1, 0) == 0 && checkHealth(player1, 1) == 0 && checkHealth(player1, 2) == 0 {
+        game.winner = player2
+        game.state = .isOver
+    }
+    if checkHealth(player2, 0) == 0 && checkHealth(player2, 1) == 0 && checkHealth(player2, 2) == 0 {
+        game.state = .isOver
+        game.winner = player1
+    }
+}
+
+private func attack() {
+    currentC!.attack(ennemy: currentTarget!)
+    // set life on 0 ( cannot be negative )
+    if (currentTarget?.race.health)! < 0 {
+        currentTarget?.race.health = 0
+    }
+}
+
+private func heal() {
+    currentC!.heal(ally: currentTarget!)
+    // set life on max even if it heal more
+    if (currentTarget?.race.health)! > (currentTarget?.race.healthMax)! {
+        currentTarget?.race.health = (currentTarget?.race.healthMax)!
+    }
+}
+
+private func checkTurn() {
+    var characterPlayed = 0
+    for character in characterArray where character.canPlay == false {
+        characterPlayed += 1
+    }
+    if characterPlayed == 6 {
+        game.totalTurn += 1
+        currentPIndex = 0
+        for character in characterArray where character.race.health > 0 {
+            character.canPlay = true
+        }
+    }
+}
+
+// convert the type race of characters to an emoji for the label button
+private func convertRace(charac: Character) -> String {
+        var race = ""
+    if charac.race.type.rawValue == "elf" {
+            race = "🧝"
+        }
+    if charac.race.type.rawValue == "human" {
+            race = "👨"
+        }
+    if charac.race.type.rawValue == "wizzard" {
+            race = "🧙‍♂️"
+        }
+    if charac.race.type.rawValue == "dwarf" {
+            race = "🎅"
+        }
+        return race
+    }
+
+private func checkHealth(_ player: Player, _ index: Int) -> Int {
+    return player.characters[index].race.health
+}
+
+private func randomChest(view: UIViewController) {
+    let randomNumber2: Int = .random(in: 0...4)
+    let randomNumber: Int = .random(in: 0...2)
+    if randomNumber2 == 2 {
+    switch currentC!.race.type {
+    case .elf : randomWeapon = arrayBow[randomNumber]
+    case .dwarf : randomWeapon = arrayAxe[randomNumber]
+    case .human : randomWeapon = arraySword[randomNumber]
+    case .wizzard : randomWeapon = arrayStick[randomNumber]
+    }
+    currentC!.race.weapon = randomWeapon!
+    let alertController = UIAlertController(title: "🎁", message: "A treasure chest appears with a \(randomWeapon!.name) inside\ndamage : \(randomWeapon!.damage) heal : \(randomWeapon!.heal)", preferredStyle: .alert)
+    let presentChest = UIAlertAction(title: okString, style: .default, handler: {_ in
+            if game.state == .isOver {
+                view.performSegue(withIdentifier: "winView", sender: Any?.self)
+            }
+        })
+    alertController.addAction(presentChest)
+    view.present(alertController, animated: true)
     }
 }
